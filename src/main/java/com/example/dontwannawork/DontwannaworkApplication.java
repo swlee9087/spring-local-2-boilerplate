3ed5dfa6aeb165abe6f5f5ba8a0375e152a0df81
swlee9087 <sw_lee9087@naver.com>
2021-08-11 11:52:20 +0900

    first commit

---
+++ src/main/java/com/example/dontwannawork/DontwannaworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.dontwannawork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontwannaworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontwannaworkApplication.class, args);
+	}
+
+}
